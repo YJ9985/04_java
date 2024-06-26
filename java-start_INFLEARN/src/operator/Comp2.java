bcf1a098f872a595a6e2b32ab1745a4876816991
YJ9985 <lucy1486@naver.com>
2024-06-26 10:26:53 +0900

    240626 인프런 강의

---
+++ java-start_INFLEARN/src/operator/Comp2.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Comp2 {
+    String str1 = "Sentnece1";
+    String str2 = "Sentnece2";
+
+    boolean result1 = "hello".equals("hello");
+    boolean result2 = str1.equals(str2);
+    boolean result3 = str1.equals("Sentence1");
+
+
+}
