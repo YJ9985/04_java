0397350289e8d1c91c895aca3346ce1e95adba43
YJ9985 <lucy1486@naver.com>
2024-06-27 18:40:23 +0900

    240627

--- study/src/ch11/sec02/exam01/ExceptionHandlingExample1.java
+++ study/src/ch11/sec02/exam01/ExceptionHandlingExample1.java
@@ -1,2 +1,24 @@
-package ch11.sec02.exam01;public class ExceptionHandlingExample1 {
+package ch11.sec02.exam01;
+
+public class ExceptionHandlingExample1 {
+    public static void printLength(String data) {
+        try{
+            int result = data.length();
+            System.out.println("문자 수: " + result);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            System.out.println("[마무리 실행]\n");
+        }
+
+        int result = data.length(); //12번째 줄에서 null을 전달하여 null.length가 실행되어 NullPointerException이 발생한다.
+        System.out.println("문자 수: " + result);
+    }
+    public static void main(String[] args) {
+        System.out.println("[프로그램 시작]\n");
+        printLength("ThisIsJava");
+        printLength(null); // null을 printLength에 전달하여 에러 발생
+        System.out.println("[프로그램 종료]");
+    }
 }
--- study/src/ch11/sec03/exam01/ExceptionHandlingExample.java
+++ study/src/ch11/sec03/exam01/ExceptionHandlingExample.java
@@ -1,2 +1,18 @@
-package ch11.sec03.exam01;public class ExceptionHandlingExample {
+package ch11.sec03.exam01;
+
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        String[] array = {"100", "1oo"};
+        for (int i = 0; i <= array.length; i++) {
+            try {
+                int value = Integer.parseInt(array[i]);
+                System.out.println("array[" + i + "]: " + value);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("배열 크기값 초과접근: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("숫자 변환시 에러 발생: " + e.getMessage());
+            }
+
+        }
+    }
 }
