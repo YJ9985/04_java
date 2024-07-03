4196946a98b9966378787809b4ad22cf9b790aa2
YJ9985 <lucy1486@naver.com>
2024-07-03 16:07:37 +0900

    240703 퀴즈 문제풀이

--- study/src/ch11/sec03/exam03/ExceptionHandlingExample.java
+++ study/src/ch11/sec03/exam03/ExceptionHandlingExample.java
@@ -1,19 +1,19 @@
-package ch11.sec03.exam01;
-
-public class ExceptionHandlingExample {
-    public static void main(String[] args) {
-        String[] array = {"100", "1oo", null, "200"};
-
-        for (int i = 0; i <= array.length; i++) {
-            try {
-                int value = Integer.parseInt(array[i]);
-                System.out.println("array[" + i + "]: " + value);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("배열 크기값 초과접근: " + e.getMessage());
-            } catch (NumberFormatException | NullPointerException e) {
-                System.out.println("데이터에 문제가 있음: " + e.getMessage());
-            }
-
-        }
-    }
-}
+//package ch11.sec03.exam01;
+//
+//public class ExceptionHandlingExample {
+//    public static void main(String[] args) {
+//        String[] array = {"100", "1oo", null, "200"};
+//
+//        for (int i = 0; i <= array.length; i++) {
+//            try {
+//                int value = Integer.parseInt(array[i]);
+//                System.out.println("array[" + i + "]: " + value);
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                System.out.println("배열 크기값 초과접근: " + e.getMessage());
+//            } catch (NumberFormatException | NullPointerException e) {
+//                System.out.println("데이터에 문제가 있음: " + e.getMessage());
+//            }
+//
+//        }
+//    }
+//}
