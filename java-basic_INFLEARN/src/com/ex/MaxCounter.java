2676d3b902d31005e5ddd27b3a2206a7c3da6ce4
YJ9985 <lucy1486@naver.com>
2024-08-13 13:54:40 +0900

    240813 인프런 강의

---
+++ java-basic_INFLEARN/src/com/ex/MaxCounter.java
@@ -0,0 +1,23 @@
+package com.ex;
+
+public class MaxCounter {
+    private int count = 0;
+    private int max;
+
+    public MaxCounter(int max) {
+        this.max = max;
+    }
+
+
+    public void increment() {
+        if (count < max) {
+            count++;
+        } else {
+            System.out.println("최댓값 " + max + " 이상 숫자를 증가할 수 없습니다.");
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
