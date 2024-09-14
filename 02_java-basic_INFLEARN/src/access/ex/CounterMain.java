46540ef9c7266cb454be92e9fe853ac0cefea6a6
YJ9985 <lucy1486@naver.com>
2024-09-14 16:04:59 +0900

    Update CounterMain.java

--- 02_java-basic_INFLEARN/src/access/ex/CounterMain.java
+++ 02_java-basic_INFLEARN/src/access/ex/CounterMain.java
@@ -1,6 +1,7 @@
 package access.ex;
 
 public class CounterMain {
+    //복습
     public static void main(String[] args) {
         MaxCounter counter = new MaxCounter(3);
         counter.increment();
