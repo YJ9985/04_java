63291e406b56266a9082dbedb07c27ec15ecf091
YJ9985 <lucy1486@naver.com>
2024-09-27 16:04:56 +0900

    240927 중복 파일 삭제

--- java-basic_INFLEARN/src/class1/MovieReview.java
+++
@@ -1,6 +0,0 @@
-package class1;
-
-public class MovieReview {
-    String title;
-    String review;
-}
--- java-basic_INFLEARN/src/pack/a/User2.java
+++
@@ -1,7 +0,0 @@
-package pack.a;
-
-public class User2 {
-    public User2() {
-        System.out.println("패키지 pack.a User2 생성");
-    }
-}
