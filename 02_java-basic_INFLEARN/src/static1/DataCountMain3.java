0b0bb7c64ae9ad365f72688466427ddd29b049bf
YJ9985 <lucy1486@naver.com>
2024-10-14 10:41:00 +0000

    241014

--- 02_java-basic_INFLEARN/src/static1/DataCountMain3.java
+++ 02_java-basic_INFLEARN/src/static1/DataCountMain3.java
@@ -1,7 +1,7 @@
 package static1;
 
 public class DataCountMain3 {
-    //얍
+    //얍얍
     public static void main(String[] args) {
         Data3 data1 = new Data3("A");
         System.out.println("A count = " + Data3.count);
